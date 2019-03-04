23bdcd3736624fb2a74d6cedaa0f1d17e7e68412
Joe Previte <jjprevite@gmail.com>
2019-03-03 18:36:01 -0700

    Add Typography.js to help with fonts and styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '10px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Ubuntu Mono', 'monospace'],
+  bodyFontFamily: ['Fire Mono', 'monospace'],
+})
+
+export default typography
