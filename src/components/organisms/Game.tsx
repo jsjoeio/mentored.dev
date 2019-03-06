d75dab1b06ab72f1a0fab0b82466bcecd213301a
Joe Previte <jjprevite@gmail.com>
2019-03-05 20:31:02 -0700

    Refactor functionality in Game to store input in state for name

--- src/components/organisms/Game.tsx
+++ src/components/organisms/Game.tsx
@@ -1,40 +1,46 @@
-import React, { useState, useReducer, useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import story from '../../utils/story'
 import Form from '../molecules/Form'
+import { initialState, storyInputsReducer } from '../../state/storyInputs'
+import { getMessage } from '../../utils/functions'
 
 const Game = ({}) => {
-  const [localState, setLocalState] = useState({})
-  const transitionStory = (state, transition) => {
-    // 1. if INPUT, create new key in object...
-    // 2. if HAS_KEY, need to modify MESSAGE before returning...
-    return story.states[state[transition]] || state
-  }
+  const [state, dispatch] = useReducer(storyInputsReducer, initialState)
+  const transitionStory = (state, transition) =>
+    story.states[state[transition]] || state
   const [storyState, transitionTo] = useReducer(
     transitionStory,
     story.states.initial
   )
   useEffect(() => {
+    // If INPUT, create new key in "story input state"
     if (storyState.hasOwnProperty('INPUT')) {
-      const newKey = {
-        [storyState['INPUT']['KEY']]: ''
-      }
-      setLocalState(prevState => ({ ...prevState, ...newKey }))
+      const key = storyState['INPUT']['KEY']
+      dispatch({ type: 'ADD_INPUT_KEY', key })
     }
-    console.log(localState, 'here ya go joe')
   }, [storyState])
+
+  const handleOnChange = e => {
+    dispatch({
+      type: 'UPDATE_INPUT_KEY',
+      key: storyState['INPUT']['KEY'],
+      value: e.target.value
+    })
+  }
+  let message
+  if (storyState['HAS_KEY']) {
+    message = getMessage(state, storyState)
+  } else {
+    message = storyState['MESSAGE']
+  }
   return (
     <React.Fragment>
-      <p style={{ fontSize: '3rem' }}>{storyState['MESSAGE']}</p>
+      <p style={{ fontSize: '3rem', whiteSpace: 'pre' }}>{message}</p>
       {storyState.hasOwnProperty('INPUT') && (
         <Form
           for={storyState['INPUT']['KEY']}
-          value={localState[storyState['INPUT']['KEY']]}
-          onChange={e =>
-            setLocalState({
-              ...localState,
-              [storyState['INPUT']['KEY']]: e.target.value
-            })
-          }
+          value={state[storyState['INPUT']['KEY']] || ''}
+          handleOnChange={e => handleOnChange(e)}
           handleSubmit={e => {
             e.preventDefault()
           }}
