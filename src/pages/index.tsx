433da3620bad16f28a18fbd9336f3084dc3c2e26
Joe Previte <jjprevite@gmail.com>
2019-03-05 18:51:31 -0700

    Refactor Game into organism component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useReducer } from 'react'
+import React, { useState, useReducer, useEffect } from 'react'
 import { graphql } from 'gatsby'
-import story from '../utils/story'
-import Form from '../components/molecules/Form'
+import Game from '../components/organisms/Game'
 
 interface IndexPageProps {
   data: {
@@ -15,39 +14,10 @@ interface IndexPageProps {
 
 const Index = (props: IndexPageProps) => {
   const [gameStarted, startGame] = useState(false)
-  const [state, setState] = useState({ name: '' })
-  const transitionStory = (state, transition) => {
-    if (state['NEXT'] === 'greet') {
-      story.states[state[transition]].name = 'Joe.'
-    }
-    return story.states[state[transition]] || state
-  }
-  const [storyState, transitionTo] = useReducer(
-    transitionStory,
-    story.states.initial
-  )
-  console.log('this is state', state)
   return (
     <div>
       {!gameStarted && <h2 onClick={() => startGame(!gameStarted)}>Enter</h2>}
-      {gameStarted && (
-        <React.Fragment>
-          <p style={{ fontSize: '3rem' }}>{storyState['MESSAGE']}</p>
-          {storyState.hasOwnProperty('VALUE') && (
-            <Form
-              for="name"
-              value={state.name}
-              onChange={e => setState({ ...state, name: e.target.value })}
-              handleSubmit={e => {
-                e.preventDefault()
-              }}
-            />
-          )}
-          <div>
-            <button onClick={() => transitionTo('NEXT')}>Next</button>
-          </div>
-        </React.Fragment>
-      )}
+      {gameStarted && <Game />}
     </div>
   )
 }
