ceab6e005345f89ed095cef44813af1ade7f678e
Joe Previte <jjprevite@gmail.com>
2019-03-07 19:59:06 -0700

    Refactor Form to conditionally show text/radio

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import InputText from '../atoms/InputText'
+import InputRadio from '../atoms/InputRadio'
 
-const Form = ({ name, handleOnChange, value }) => {
+const Form = ({ state, input, handleOnChange, value }) => {
   return (
     <form>
-      <InputText name={name} value={value} onChange={handleOnChange} />
+      {input['TYPE'] === 'text' && (
+        <InputText
+          questionKey={input['KEY']}
+          value={value}
+          onChange={handleOnChange}
+        />
+      )}
+      {input['TYPE'] === 'radio' && (
+        <InputRadio
+          currentValue={state[input['KEY']]}
+          options={input['OPTIONS']}
+          questionKey={input['KEY']}
+          value={value}
+          onChange={handleOnChange}
+        />
+      )}
     </form>
   )
 }
