bd3c5b5fad602cc5eec5933a655d082d3c14b67e
Joe Previte <jjprevite@gmail.com>
2019-03-09 11:26:33 -0700

    Add logic to use arrow key and enter for Inputs

--- src/components/atoms/InputText.tsx
+++ src/components/atoms/InputText.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-const InputText = ({ questionKey, value, onChange }) => (
-  <React.Fragment>
-    <label htmlFor={questionKey}>{questionKey}</label>
-    <input
-      value={value}
-      onChange={onChange}
-      type="text"
-      name={questionKey}
-      id={questionKey}
-    />
-  </React.Fragment>
-)
+const InputText = ({ questionKey, value, onChange, transition }) => {
+  const inputTextRef = useRef(null)
+  useEffect(() => {
+    inputTextRef.current.focus()
+  }, [inputTextRef])
+  return (
+    <React.Fragment>
+      <label htmlFor={questionKey}>{questionKey}</label>
+      <input
+        ref={inputTextRef}
+        tabIndex="1"
+        value={value}
+        onChange={onChange}
+        onKeyUp={e => {
+          if (e.keyCode === 13 && value !== '') {
+            transition()
+          }
+        }}
+        type="text"
+        name={questionKey}
+        id={questionKey}
+      />
+    </React.Fragment>
+  )
+}
 
 export default InputText
