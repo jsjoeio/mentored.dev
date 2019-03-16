59dde2998ab692859aaf7a9b24d1d7871f1ca3b5
Joe Previte <jjprevite@gmail.com>
2019-03-15 19:22:14 -0700

    Add function to calculateScore

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -10,3 +10,18 @@ export function getMessage(state, storyState) {
   // Replace value in message and return message
   return storyState['MESSAGE'].replace(`{${key}}`, value)
 }
+
+/**
+ * @description Calculates the the score using state and an answerKey.
+ * @param state {object}
+ * @param answerKey {object}
+ * @returns {string} returns a string with the score over the total possible (i.e. 2/3)
+ */
+export function calculateScore(state, answerKey) {
+  // I need to compare the
+  let totalScore = 0
+  let totalPossible = 0
+  // loop over the answerKey grabbing the key from state, comparing value to answer value,
+  // if correct, increment total score
+  return `${score}/${totalPossible}`
+}
