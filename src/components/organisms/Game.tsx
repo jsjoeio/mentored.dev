0e1b742e3ecbd760df6faaa6877fea65ea64bad3
Joe Previte <jjprevite@gmail.com>
2019-03-15 19:02:12 -0700

    Add functionality for dynamic NEXT states

--- src/components/organisms/Game.tsx
+++ src/components/organisms/Game.tsx
@@ -5,10 +5,28 @@ import story from '../../utils/story'
 import Form from '../molecules/Form'
 import Dialog from './Dialog'
 
-const Game = ({}) => {
+const Game = () => {
   const [state, dispatch] = useReducer(storyInputsReducer, initialState)
-  const transitionStory = (state, transition) =>
-    story.states[state[transition]] || state
+  const transitionStory = (storyState, transition) => {
+    if (typeof storyState[transition] === 'string') {
+      return story.states[storyState[transition]] || storyState
+    } else if (typeof storyState[transition] === 'object') {
+      // Grab what they choose for the question from state
+      const optionSelected = state[storyState['INPUT']['KEY']]
+      // Loop through options to find key
+      let keyOfmatchingOption
+      for (let i = 0; i < storyState['INPUT']['OPTIONS'].length; i++) {
+        if (storyState['INPUT']['OPTIONS'][i] === optionSelected) {
+          keyOfmatchingOption = i
+          break
+        }
+      }
+      return (
+        story.states[storyState[transition][`OPTION_${keyOfmatchingOption}`]] ||
+        storyState
+      )
+    }
+  }
   const [storyState, transitionTo] = useReducer(
     transitionStory,
     story.states.initial
@@ -29,6 +47,7 @@ const Game = ({}) => {
     })
   }
   let message
+  console.log('hi storystate', storyState)
   if (storyState['HAS_KEY']) {
     message = getMessage(state, storyState)
   } else {
