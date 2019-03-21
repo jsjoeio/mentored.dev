684ab5f0f5ddc8f6ae2f2579ff96a89dd185ae63
Joe Previte <jjprevite@gmail.com>
2019-03-20 20:40:27 -0700

    Add theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,32 @@
+const lightTheme = {
+  primary: {
+    darker: '#2A2A2A',
+    main: '#474747'
+  },
+  secondary: {
+    main: '#ffffff'
+  },
+  background: {
+    main: '#FBF5EC'
+  }
+}
+
+const darkTheme = {
+  primary: {
+    main: '#FBF5EC'
+  },
+  secondary: {
+    main: '#000'
+  },
+  background: {
+    darker: '#2A2A2A',
+    main: '#474747'
+  }
+}
+
+const theme = {
+  lightTheme,
+  darkTheme
+}
+
+export default theme
