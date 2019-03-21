99dba7141edbe6212678a8531af1510f325699a3
Joe Previte <jjprevite@gmail.com>
2019-03-20 20:40:20 -0700

    Update fonts

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,10 +1,20 @@
 import Typography from 'typography'
 
 const typography = new Typography({
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: ['400']
+    },
+    {
+      name: 'Bai Jamjuree',
+      styles: ['400']
+    }
+  ],
   baseFontSize: '10px',
   baseLineHeight: 1.666,
-  headerFontFamily: ['Ubuntu Mono', 'monospace'],
-  bodyFontFamily: ['Fire Mono', 'monospace'],
+  headerFontFamily: ['Montserrat', 'sans-serif'],
+  bodyFontFamily: ['Bai Jamjuree', 'sans-serif']
 })
 
 export default typography
