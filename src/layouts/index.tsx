3996333a0b256f19a2981eabf3344eaaf0780e47
Joe Previte <jjprevite@gmail.com>
2019-03-20 20:40:10 -0700

    Add Layout plugin and component with theme

---
+++ src/layouts/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from '../utils/theme'
+
+const Layout = ({ children }) => {
+  // adding ability to set theme, but Layout only likes one child.
+  // WIll have to figure out later @TechDebt
+  const [darkTheme, setDarkTheme] = useState(false)
+  console.log(theme.lightTheme)
+  return (
+    <ThemeProvider theme={darkTheme ? theme.darkTheme : theme.lightTheme}>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default Layout
