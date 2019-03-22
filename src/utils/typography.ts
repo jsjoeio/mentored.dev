0902d054a77457033494a67f0f895c5081eab2e7
Joe Previte <jjprevite@gmail.com>
2019-03-21 20:49:20 -0700

    Remove Monseratt font

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -3,17 +3,13 @@ import Typography from 'typography'
 const typography = new Typography({
   googleFonts: [
     {
-      name: 'Montserrat',
-      styles: ['400']
-    },
-    {
       name: 'Bai Jamjuree',
-      styles: ['400']
+      styles: ['400', '700']
     }
   ],
   baseFontSize: '10px',
   baseLineHeight: 1.666,
-  headerFontFamily: ['Montserrat', 'sans-serif'],
+  headerFontFamily: ['Bai Jamjuree', 'sans-serif'],
   bodyFontFamily: ['Bai Jamjuree', 'sans-serif']
 })
 
