d4add231d64d8a8026ffe6f2725e9393fa223e79
Joe Previte <jjprevite@gmail.com>
2019-03-23 10:09:10 -0700

    Add hideElement mixin

---
+++ src/utils/mixins.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-components'
+
+export const hideElement = () => css`
+  border: 0;
+  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+  clip; rect(1px, 1px, 1px, 1px);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+`
