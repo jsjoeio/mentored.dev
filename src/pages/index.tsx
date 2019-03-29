ce9a9b2648e34f0f947b06dfb092a4d1df3a3a45
Joe Previte <jjprevite@gmail.com>
2019-03-28 18:14:40 -0700

    Create landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useReducer, useEffect, useRef } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 import { graphql } from 'gatsby'
-import Game from '../components/organisms/Game'
-import Enter from '../components/atoms/Enter'
+import Laptop from '../components/atoms/Laptop'
+
 interface IndexPageProps {
   data: {
     site: {
@@ -12,22 +13,56 @@ interface IndexPageProps {
   }
 }
 
+const Container = styled.div`
+  text-align: center;
+  margin: auto;
+  height: 80vh;
+  width: 100%;
+`
+
+const Title = styled.h1`
+  margin-top: 20vh;
+  font-size: 15vh;
+  font-weight: bold;
+  text-transform: lowercase;
+
+  @media only screen and (max-width: 600px) {
+    margin-top: 10vh;
+    font-size: 8vh;
+  }
+`
+
+const SubTitle = styled.p`
+  margin-top: 2vh;
+  font-size: 3vh;
+`
+
+const Instructions = styled.p`
+  margin-top: 4vh;
+  font-size: 3vh;
+`
+
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  height: 4vh;
+  font-size: 1.75vh;
+  text-transform: uppercase;
+  text-align: center;
+`
+
 const Index = (props: IndexPageProps) => {
-  const [gameStarted, startGame] = useState(false)
   return (
-    <div>
-      {!gameStarted && (
-        <div
-          style={{
-            margin: '50vh auto',
-            textAlign: 'center'
-          }}
-        >
-          <Enter onClick={() => startGame(!gameStarted)} />
-        </div>
-      )}
-      {gameStarted && <Game />}
-    </div>
+    <Container>
+      <Title>mentored.dev</Title>
+      <SubTitle>The best game for learning web development</SubTitle>
+      <div>
+        <Instructions>Press enter to play</Instructions>
+        <Laptop />
+      </div>
+      <Footer>© ‘19 jsjoeio</Footer>
+    </Container>
   )
 }
 
