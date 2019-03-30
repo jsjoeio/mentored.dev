86c0ecb8d9d846b18d833398830019e7318946ef
Joe Previte <jjprevite@gmail.com>
2019-03-30 09:04:43 -0700

    Add comment about window undefined check

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -5,6 +5,7 @@ let auth
 export const appId = process.env.GATSBY_OG_APP_ID
 
 export default () => {
+  // Prevents build from failing at runtime because window is not defined
   if (typeof window !== `undefined`) {
     if (!auth) {
       auth = new OneGraphAuth({
