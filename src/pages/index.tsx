9feb1760ecbceff8b753d79f762ac0133b1c5fbb
Joe Previte <jjprevite@gmail.com>
2019-03-30 08:41:02 -0700

    Add Username to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,26 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { graphql } from 'gatsby'
 import StartScreen from '../components/molecules/StartScreen'
+import Username from '../components/atoms/Username'
 
 const Index = ({ auth }) => {
   const [authenticated, setAuthenticated] = useState(false)
-
   function login(service = 'github') {
     return async () => {
       await auth.login(service)
-
       setAuthenticated(await auth.isLoggedIn('github'))
     }
   }
+  console.log(auth, '<---')
   return (
     <React.Fragment>
       {authenticated ? (
-        <div>
-          <h1>Logged In</h1>
-          <p>
-            Go to <Link to="/dashboard">Dashboard</Link>
+        <div style={{ textAlign: 'center', margin: 'auto', marginTop: '12vh' }}>
+          <h1 style={{ fontSize: '4vh' }}>Logged in with GitHub</h1>
+          <p style={{ fontSize: '2.5vh' }}>
+            Go to <Link to="/dashboard">Game</Link>
           </p>
+          <Username />
         </div>
       ) : (
         <StartScreen login={login()} />
