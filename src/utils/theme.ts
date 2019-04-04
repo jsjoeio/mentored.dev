975bb8282e7d6be32cceef45f51c56a8003cd330
Joe Previte <jjprevite@gmail.com>
2019-04-03 20:17:05 -0700

    Update theme  - add accentBlue and primary

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,71 +1,131 @@
+export interface Color {
+  /**
+   * 900
+   */
+  darkest: string
+  /**
+   * 800
+   */
+  darker: string
+  /**
+   * 700
+   */
+  dark: string
+  /**
+   * 600
+   */
+  main: string
+  /**
+   * 500
+   */
+  light: string
+  /**
+   * 400
+   */
+  lighter: string
+  /**
+   * 300
+   */
+  lighest: string
+}
+
 export interface Theme {
   primary: {
-    darker: string;
-    main: string;
+    darker: string
+    main: string
     /**
      * # LIGHTER PRIMARY COLOR
      * ![horse wat](https://media.giphy.com/media/xULW8Fes0Tau8uOic0/giphy.gif)
      *  - light theme: "rgba(71,71,71, 0.4)",
      *  - dark theme: unimplemented!
      */
-    lighter: string;
-  };
+    lighter: string
+  }
   secondary: {
-    main: string;
-  };
+    main: string
+  }
   background: {
-    main: string;
-    darker: string;
-  };
+    main: string
+    darker: string
+  }
+  accentBlue: Color
+  neutral: Color
   focus: {
-    main: string;
-    lighter: string;
-  };
+    main: string
+    lighter: string
+  }
 }
 
 const lightTheme: Theme = {
   primary: {
-    darker: "#2A2A2A",
-    main: "#474747",
-    lighter: "rgba(71,71,71, 0.4)",
+    darker: '#2A2A2A',
+    main: '#474747',
+    lighter: 'rgba(71,71,71, 0.4)'
   },
   secondary: {
-    main: "#ffffff",
+    main: '#ffffff'
   },
   background: {
-    main: "#F3F3F3",
-    darker: "",
+    main: '#F3F3F3',
+    darker: ''
+  },
+  accentBlue: {
+    darkest: '#0A343D',
+    darker: '#14697A',
+    dark: '#1B8CA3',
+    main: '#22AFCC',
+    light: '#64C7DB',
+    lighter: '#A6DFEA',
+    lighest: '#D2EFF4'
+  },
+  neutral: {
+    darkest: '#2D2D2D',
+    darker: '#474747',
+    dark: '#797979',
+    main: '#B2B2B2',
+    light: '#DFDFDF',
+    lighter: '#F3F3F3',
+    lighest: '#FAFAFA'
   },
   focus: {
-    main: "#2574A9",
-    lighter: "rgba(37,116,169, 0.3)",
-  },
-};
+    main: '#2574A9',
+    lighter: 'rgba(37,116,169, 0.3)'
+  }
+}
 
 const darkTheme: Theme = {
   focus: {
-    lighter: "",
-    main: "",
+    lighter: '',
+    main: ''
   },
   primary: {
-    main: "#F3F3F3",
-    darker: "",
-    lighter: "",
+    main: '#F3F3F3',
+    darker: '',
+    lighter: ''
   },
   secondary: {
-    main: "#000",
+    main: '#000'
   },
   background: {
-    darker: "#2A2A2A",
-    main: "#474747",
+    darker: '#2A2A2A',
+    main: '#474747'
   },
-};
+  accentBlue: {
+    darkest: '0A343D',
+    darker: '14697A',
+    dark: '1B8CA3',
+    main: '22AFCC',
+    light: '64C7DB',
+    lighter: 'A6DFEA',
+    lighest: 'D2EFF4'
+  }
+}
 
 const theme = {
   lightTheme,
-  darkTheme,
-};
+  darkTheme
+}
 
 // export type Theme = typeof theme;
 
-export default theme;
+export default theme
