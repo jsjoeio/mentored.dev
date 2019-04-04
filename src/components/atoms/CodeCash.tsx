cd45d653e206f64acac47e07d0996f03f9af6340
Joe Previte <jjprevite@gmail.com>
2019-04-03 20:17:23 -0700

    Create CodeCash component

---
+++ src/components/atoms/CodeCash.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from '../../utils/styled'
+import IconCoin from './IconCoin'
+
+const Container = styled.div`
+  display: flex;
+  margin-left: 25px;
+  p {
+    margin-top: 4px;
+    margin-left: 8px;
+    display: inline-block;
+    font-size: 18px;
+  }
+`
+
+const CodeCash = () => (
+  <Container>
+    <IconCoin />
+    <p>$200</p>
+  </Container>
+)
+
+export default CodeCash
