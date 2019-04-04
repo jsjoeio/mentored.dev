1c1636d95ccf0f61aff0c2e6e3dacbd1594059d4
Joe Previte <jjprevite@gmail.com>
2019-04-03 18:35:05 -0700

    Add Icons

---
+++ src/components/atoms/IconCoin.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+// TODO - change fills to use theme colors
+const IconCoin = () => (
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="15.5" cy="15.4998" r="13.5" fill="#F3C13A" />
+    <path
+      d="M23.4319 5.56571C20.9782 3.22717 17.6961 1.95914 14.3074 2.04056C10.9188 2.12197 7.70137 3.54618 5.36283 5.99985C3.02429 8.45353 1.75626 11.7357 1.83768 15.1243C1.91909 18.5129 3.34329 21.7304 5.79697 24.0689L23.4319 5.56571Z"
+      fill="#FBECC3"
+    />
+    <circle
+      cx="15.5003"
+      cy="15.4998"
+      r="13.5833"
+      stroke="#C29B2E"
+      stroke-width="2"
+    />
+    <path
+      d="M12.0415 13.6802L8.2085 15.3837L12.0415 17.0873"
+      stroke="#C29B2E"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M17.5005 17.0874L21.3335 15.3839L17.5005 13.6803"
+      stroke="#C29B2E"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <line
+      x1="15.8608"
+      y1="11.767"
+      x2="13.2253"
+      y2="18.9205"
+      stroke="#C29B2E"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+)
+
+export default IconCoin
