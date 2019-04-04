da15a4ee5af67cc3db873f5aca28112b207cdaa8
Joe Previte <jjprevite@gmail.com>
2019-04-03 20:19:12 -0700

    Add Profile to Dashboard

---
+++ src/components/organisms/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Username from '../atoms/Username'
+import Profile from './Profile'
+
+const Dashboard = () => (
+  <div style={{ marginTop: '12vh' }}>
+    <Profile />
+    <p style={{ fontSize: '2.5vh' }}>
+      Go to <Link to="/dashboard">Game</Link>
+    </p>
+  </div>
+)
+
+export default Dashboard
