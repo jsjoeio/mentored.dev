85e1ce1c65ae7dd8aedc70e5bbd7a6f628205e18
Joe Previte <jjprevite@gmail.com>
2019-04-04 19:54:04 -0700

    Add Achievement components

---
+++ src/components/atoms/AchievementCommandLine.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AchievementCommandLine = () => (
+  <svg
+    width="65"
+    height="81"
+    viewBox="0 0 61 76"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 15C0 6.71573 6.71573 0 15 0H30.5H46C54.2843 0 61 6.71573 61 15V51.7773C61 57.5174 57.7242 62.7542 52.5626 65.2656L37.0626 72.807C32.9197 74.8227 28.0803 74.8227 23.9374 72.807L8.43738 65.2655C3.27579 62.7542 0 57.5174 0 51.7773V15Z"
+      fill="#2D2D2D"
+    />
+    <path
+      d="M4 20C4 11.7157 10.7157 5 19 5H30.5H42C50.2843 5 57 11.7157 57 20V48.7279C57 54.469 53.723 59.7066 48.56 62.2174L37.06 67.8099C32.9185 69.8239 28.0815 69.8239 23.94 67.8099L12.44 62.2174C7.277 59.7066 4 54.469 4 48.7279V20Z"
+      fill="#474747"
+    />
+    <path
+      d="M10.5 21.5L26.5 30L10.5 38"
+      stroke="#FAFAFA"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M29 44H49.5"
+      stroke="#FAFAFA"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+)
+
+export default AchievementCommandLine
