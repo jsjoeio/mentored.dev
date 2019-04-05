5669e74fc5619a81e9ec8b7a3fadf39583a42119
Joe Previte <jjprevite@gmail.com>
2019-04-04 19:54:14 -0700

    Fix styles in Container used in Sidebar

--- src/components/organisms/Profile.tsx
+++ src/components/organisms/Profile.tsx
@@ -5,9 +5,13 @@ import XPBar from '../molecules/XPBar'
 import ProfileStatBars from '../molecules/ProfileStatBars'
 import ProfileHeader from '../molecules/ProfileHeader'
 
-const Container = styled.div`
-  width: 475px;
+export const Container = styled.div`
+  width: 100%;
   margin-left: auto;
+
+  h3 {
+    font-size: 2.75rem;
+  }
 `
 
 const Profile = () => (
