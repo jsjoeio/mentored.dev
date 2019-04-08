995e3e02e727e6494cd537b7003ff50a3e03c581
Joe Previte <jjprevite@gmail.com>
2019-04-08 15:42:57 -0700

    Add AudioPlayer with Icons

---
+++ src/components/atoms/AudioPlayer.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+import styled from '../../utils/styled'
+import IconSoundOn from './IconSoundOn'
+import IconSoundOff from './IconSoundOff'
+
+const AudioContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  margin: 2.65vh 5px;
+
+  svg {
+    transition: all 0.3s ease;
+    border: 3px solid transparent;
+    border-radius: 50%;
+    height: 4rem;
+    width: 4rem;
+  }
+`
+
+const Button = styled.button`
+  box-sizing: border-box;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  &:hover,
+  &:focus,
+  &:active {
+    cursor: pointer;
+    outline: none;
+
+    svg {
+      border: 3px solid ${props => props.theme.focus.main};
+      border-radius: 50%;
+    }
+  }
+`
+
+const useAudio = (url: string) => {
+  const [audio] = useState(new Audio(url))
+  const [playing, setPlaying] = useState(true)
+
+  const toggle = () => setPlaying(!playing)
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause()
+    audio.loop = true
+    return () => {
+      audio.pause()
+    }
+  }, [playing])
+
+  return [playing, toggle]
+}
+
+const AudioPlayer: React.FC<{ url: string }> = ({ url }) => {
+  const [playing, toggle] = useAudio(url)
+
+  return (
+    <AudioContainer>
+      <Button onClick={toggle}>
+        {playing ? <IconSoundOn /> : <IconSoundOff />}
+      </Button>
+    </AudioContainer>
+  )
+}
+
+export default AudioPlayer
