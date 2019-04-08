905fab143388a69c594d4544bbb00a10c1380755
Joe Previte <jjprevite@gmail.com>
2019-04-08 16:30:41 -0700

    Fix theme error

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -35,7 +35,6 @@ export interface Theme {
     main: string
     /**
      * # LIGHTER PRIMARY COLOR
-     * ![horse wat](https://media.giphy.com/media/xULW8Fes0Tau8uOic0/giphy.gif)
      *  - light theme: "rgba(71,71,71, 0.4)",
      *  - dark theme: unimplemented!
      */
@@ -94,30 +93,39 @@ const lightTheme: Theme = {
 }
 
 const darkTheme: Theme = {
-  focus: {
-    lighter: '',
-    main: ''
-  },
   primary: {
-    main: '#F3F3F3',
-    darker: '',
-    lighter: ''
+    darker: '#2A2A2A',
+    main: '#474747',
+    lighter: 'rgba(71,71,71, 0.4)'
   },
   secondary: {
-    main: '#000'
+    main: '#ffffff'
   },
   background: {
-    darker: '#2A2A2A',
-    main: '#474747'
+    main: '#F3F3F3',
+    darker: ''
   },
   accentBlue: {
-    darkest: '0A343D',
-    darker: '14697A',
-    dark: '1B8CA3',
-    main: '22AFCC',
-    light: '64C7DB',
-    lighter: 'A6DFEA',
-    lightest: 'D2EFF4'
+    darkest: '#0A343D',
+    darker: '#14697A',
+    dark: '#1B8CA3',
+    main: '#22AFCC',
+    light: '#64C7DB',
+    lighter: '#A6DFEA',
+    lightest: '#D2EFF4'
+  },
+  neutral: {
+    darkest: '#2D2D2D',
+    darker: '#474747',
+    dark: '#797979',
+    main: '#B2B2B2',
+    light: '#DFDFDF',
+    lighter: '#F3F3F3',
+    lightest: '#FAFAFA'
+  },
+  focus: {
+    main: '#2574A9',
+    lighter: 'rgba(37,116,169, 0.3)'
   }
 }
 
