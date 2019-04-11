6e0a5ef11b36a062bddac4b413a21dfb8e24748f
Joe Previte <jjprevite@gmail.com>
2019-04-11 16:38:37 -0700

    Create LogoCarrotsAnimation

---
+++ src/components/atoms/LogosCarrots/LogoCarrotsFrameOne.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const LogoFrameOne = () => (
+  <React.Fragment>
+    <path
+      d="M55 45L40 53.5333L55 61"
+      stroke="#474747"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M60 61L75 52.4667L60 45"
+      stroke="#474747"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </React.Fragment>
+)
+
+export default LogoFrameOne
