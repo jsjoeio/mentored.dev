85dcf0d4f812ab66e8a11e97f6baa3b82758b010
Joe Previte <jjprevite@gmail.com>
2019-05-08 20:26:01 -0700

    Create ExitSign

---
+++ src/components/atoms/ExitSign.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '../../utils/styled'
+
+const ExitSign = styled.g`
+  &:hover,
+  &:active,
+  &:focus {
+    cursor: pointer;
+  }
+`
+
+export default ExitSign
