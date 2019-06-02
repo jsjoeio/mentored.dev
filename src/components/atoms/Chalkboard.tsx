9341f1f40eaca528f4c3a72c5fe57bd588736402
Joe Previte <jjprevite@gmail.com>
2019-06-01 17:53:38 -0700

    Add svg image element to chalkboard

--- src/components/atoms/Chalkboard.tsx
+++ src/components/atoms/Chalkboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { fadeIn } from '../../utils/mixins'
 
 const Container = styled.div`
   position: absolute;
@@ -16,10 +17,17 @@ const Container = styled.div`
     #white-chalk {
       fill: ${props => props.theme.neutral.lightest};
     }
+
+    #lesson-visual {
+      transition: all 0.3s ease;
+      animation: ${fadeIn} 500ms ease;
+    }
   }
 `
-
-const Chalkboard = () => (
+const Chalkboard: React.FC<{ hasImage?: boolean; image?: string }> = ({
+  hasImage = false,
+  image = 'https://previews.123rf.com/images/sonyaillustration/sonyaillustration1504/sonyaillustration150400174/38329399-chalkboard-drawing-of-light-bulb-.jpg'
+}) => (
   <Container>
     <svg
       width="957"
@@ -37,6 +45,17 @@ const Chalkboard = () => (
         width="35.6445"
         height="7.37277"
       />
+      {hasImage && (
+        <svg
+          id="lesson-visual"
+          width="957"
+          height="478"
+          viewBox="0 0 957 478"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <image xlinkHref={image} height="400" width="400" x="288.5" y="22" />
+        </svg>
+      )}
     </svg>
   </Container>
 )
